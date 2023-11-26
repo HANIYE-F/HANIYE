d421d06ed5c8dd597d579ed9a4a6ae9ee8b3d615
HANIYE-F <fekrihaniye02@gmail.com>
2023-11-26 22:23:57 +0330

    three

--- halghavi.cpp
+++ halghavi.cpp
@@ -12,3 +12,12 @@ public:
     front = -1;
     rear = -1;
   }
+    bool isFull() {
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    if (front == rear + 1) {
+      return true;
+    }
+    return false;
+  }
