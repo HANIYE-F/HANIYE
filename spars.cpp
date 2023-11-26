28af8cb7e5abc7c5607150124a0744111575380e
HANIYE-F <fekrihaniye02@gmail.com>
2023-11-26 23:38:22 +0330

    two

---
+++ spars.cpp
@@ -0,0 +1,16 @@
+// Implementation of array representation
+// of the sparse array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sparse[4][4] = { { 0, 0, 7, 0 },
+                         { 1, 0, 0, 0 },
+                         { 2, 0, 5, 0 },
+                         { 0, 8, 0, 4 } };
+    int s = 0;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (sparse[i][j] != 0)
+                s++;
