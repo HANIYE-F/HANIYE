03a2d64822dbcfef7eacc34402ca9f49719ab969
HANIYE-F <fekrihaniye02@gmail.com>
2023-11-26 23:41:03 +0330

    two

--- spars.cpp
+++ spars.cpp
@@ -14,3 +14,23 @@ int main()
         for (int j = 0; j < 4; j++)
             if (sparse[i][j] != 0)
                 s++;
+
+                  int representsparse[3][s];
+    int k = 0;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (sparse[i][j] != 0) {
+                representsparse[0][k] = i;
+                representsparse[1][k] = j;
+                representsparse[2][k] = sparse[i][j];
+                k++;
+            }
+    cout << "Representation of Sparse array using arrays : "
+            "\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < s; j++)
+            cout << " " << representsparse[i][j];
+        cout << "\n";
+    }
+    return 0;
+}
