9268a1e5142dab77cca4510fe00900595e5a85fc
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-06 17:22:43 +0330

    first

---
+++ lpposhteh.cpp
@@ -0,0 +1,44 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* link;
+
+
+    Node(int n)
+    {
+        this->data = n;
+        this->link = NULL;
+    }
+};
+
+class Stack {
+    Node* top;
+
+public:
+    Stack() { top = NULL; }
+
+    void push(int data)
+    {
+
+
+        Node* temp = new Node(data);
+
+
+        if (!temp) {
+            cout << "\nStack Overflow";
+            exit(1);
+        }
+
+
+        temp->data = data;
+
+
+        temp->link = top;
+
+
+        top = temp;
+    }
