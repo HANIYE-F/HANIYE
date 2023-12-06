e8e4d785bb39f152a1e6bd788634dff481794a4a
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-06 17:17:46 +0330

    first

--- lpsaf.cpp
+++ lpsaf.cpp
@@ -30,3 +30,37 @@ struct Queue {
         rear->next = temp;
         rear = temp;
     }
+
+    void deQueue()
+    {
+
+        if (front == NULL)
+            return;
+
+        QNode* temp = front;
+        front = front->next;
+
+        if (front == NULL)
+            rear = NULL;
+
+        delete (temp);
+    }
+};
+
+/
+int main()
+{
+
+    Queue q;
+    q.enQueue(10);
+    q.enQueue(20);
+    q.deQueue();
+    q.deQueue();
+    q.enQueue(30);
+    q.enQueue(40);
+    q.enQueue(50);
+    q.deQueue();
+    cout << "Queue Front : " << ((q.front != NULL) ? (q.front)->data : -1)<< endl;
+    cout << "Queue Rear : " << ((q.rear != NULL) ? (q.rear)->data : -1);
+}
+/
