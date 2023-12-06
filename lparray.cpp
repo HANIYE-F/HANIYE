38abf35e4075761469ea535127b44152f9a1e644
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-06 17:02:35 +0330

    first

---
+++ lparray.cpp
@@ -0,0 +1,12 @@
+// C++ program to illustrate the array
+// of Linked Lists
+#include <iostream>
+using namespace std;
+
+// Structure of Linked Lists
+struct info {
+    int data;
+    info* next;
+};
+
+// Driver Code
