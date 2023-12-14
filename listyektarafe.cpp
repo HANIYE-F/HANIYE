200572130de234260b122de49eeec1b368225884
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-14 09:29:13 +0330

    first

---
+++ listyektarafe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+
+    Node(T value) : data(value), next(NULL) {}
+};
+
+template <typename T>
+class LinkedList {
+private:
+    Node<T>* head;
+    int size;
