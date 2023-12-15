fc3d7074bfb241eb5271e8c3755aa015ba18c18d
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-15 22:11:24 +0330

    three

--- listdotarafe.cpp
+++ listdotarafe.cpp
@@ -32,3 +32,31 @@ public:
         if (tail == NULL)  // اگر لیست خالی بود، گره اضافه شده به عنوان پایان لیست هم تنظیم شود
             tail = newNode;
     }
+
+      // اضافه کردن گره به انتهای لیست
+    void insertAtEnd(int data) {
+        Node* newNode = new Node();
+        newNode->data = data;
+        newNode->next = NULL;
+        newNode->prev = tail;
+
+        if (tail != NULL)
+            tail->next = newNode;
+
+        tail = newNode;
+
+        if (head == NULL)  // اگر لیست خالی بود، گره اضافه شده به عنوان ابتدای لیست هم تنظیم شود
+            head = newNode;
+    }
+    void insertofindex(int n,int data){
+    	if(is_empy())
+    		insertAtBeginning(n);
+    	if(n==1){
+    		insertAtBeginning(data);
+    		return;}
+    	if(size()>n+1 || size()<0)
+    		return;
+    	if(n==size()+1){
+    		insertAtEnd(data);
+    		return;
+    	}
