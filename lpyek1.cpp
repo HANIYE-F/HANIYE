d0a7be58ce3b082484a58e37de5bbe472541a6e9
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-15 21:26:27 +0330

    secound

--- lpyek1.cpp
+++ lpyek1.cpp
@@ -15,3 +15,29 @@ public:
     LinkedInList() {
         head = NULL;
     }
+ // Method to check if the list is empty
+    bool isEmpty() {
+        return (head == NULL);
+    }
+    // Method to insert a new node at the beginning of the list
+    void insertend(string name) {
+        if(isEmpty()){
+        	insertbegin(name);
+        	return;
+	   }
+        Node* newNode = new Node;
+        newNode->name = name;
+        Node* x=new Node;
+        x=head;
+        while(x->next!=NULL)x=x->next;
+        x->next=newNode;
+        cout << "Node inserted successfully!\n";
+    }
+
+    void insertbegin(string name) {
+        Node* newNode = new Node;
+        newNode->name = name;
+        newNode->next = head;
+        head = newNode;
+        cout << "Node inserted successfully!\n";
+    }
