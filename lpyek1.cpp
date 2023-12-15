d04dc52ea058385e4351d5485abf70b690f15d3c
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-15 21:29:16 +0330

    three

--- lpyek1.cpp
+++ lpyek1.cpp
@@ -41,3 +41,48 @@ public:
         head = newNode;
         cout << "Node inserted successfully!\n";
     }
+ void inserteonindexof(string name,int n) {
+        Node* newNode = new Node;
+        newNode->name = name;
+        Node* x=new Node;
+        x=head;
+        for(int i=0;i<n-2;i++)x=x->next;
+        newNode->next=x->next;
+        x->next=newNode;
+        cout << "Node inserted successfully!\n";
+    }
+
+    // Method to delete a node by name
+    void deleteNode(string name) {
+        if(isEmpty()){
+	   	cout << "ERROR";
+	   	return;
+	   }
+        Node* currNode = head;
+        Node* prevNode = NULL;
+        while (currNode != NULL) {
+            if (currNode->name == name) {
+                if (prevNode == NULL) {
+                    head = currNode->next;
+                } else {
+                    prevNode->next = currNode->next;
+                }
+                delete currNode;
+                cout << "Node deleted successfully!\n";
+                return;
+            }
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+        cout << "No node found with the given name!\n";
+    }
+	void delete_indexof(int n){
+	   if(isEmpty()){
+	   	cout << "ERROR";
+	   	return;
+	   }
+	   Node* x_n=new Node;
+	   x_n=head;
+	   if(n==1){
+	   	delete_begin();
+	   	cout << "Node deleted successfully!\n";
