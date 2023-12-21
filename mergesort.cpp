bcfcc60894ffbd1fb309c1b2a2a79516f98e65d1
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-21 17:57:39 +0330

    four

--- mergesort.cpp
+++ mergesort.cpp
@@ -74,4 +74,30 @@ void mergeSort(int array[], int const begin, int const end)
     mergeSort(array, mid + 1, end);
     merge(array, begin, mid, end);
 }
+// UTILITY FUNCTIONS
+// Function to print an array
+void printArray(int A[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << A[i] << " ";
+    cout << endl;
+}
 
+// Driver code
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
+    return 0;
+}
+
+// This code is contributed by Mayank Tyagi
+// This code was revised by Joshua Estes
