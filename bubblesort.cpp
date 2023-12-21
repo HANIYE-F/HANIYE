8c5b5318ae30c6efd5de6dd5ecdbce016158ed80
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-21 16:28:04 +0330

    first

---
+++ bubblesort.cpp
@@ -0,0 +1,17 @@
+// Optimized implementation of Bubble sort
+#include <bits/stdc++.h>
+using namespace std;
+
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
