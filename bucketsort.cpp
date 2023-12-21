1507191dc588c59f377ce342d9d725426c49d719
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-21 18:18:39 +0330

    three

--- bucketsort.cpp
+++ bucketsort.cpp
@@ -31,3 +31,16 @@ void bucketSort(float arr[], int n)
         for (int j = 0; j < b[i].size(); j++)
             arr[index++] = b[i][j];
 }
+// Driver program to test above function
+int main()
+{
+    float arr[]
+        = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bucketSort(arr, n);
+
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
