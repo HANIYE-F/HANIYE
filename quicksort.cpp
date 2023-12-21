9278e233189f168b8ed7e2f82b3af99d2eeb02cb
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-21 18:05:17 +0330

    three

--- quicksort.cpp
+++ quicksort.cpp
@@ -42,3 +42,17 @@ void quickSort(int arr[],int low,int high)
     quickSort(arr,pi+1,high);
   }
 }
+int main() {
+  int arr[]={10,7,8,9,1,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  // Function call
+  quickSort(arr,0,n-1);
+  //Print the sorted array
+  cout<<"Sorted Array\n";
+  for(int i=0;i<n;i++)
+  {
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
+// This Code is Contributed By Diwakar Jha
