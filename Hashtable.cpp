0a9f1e84fbb9ff979f45a95392895d4fb3402b69
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-21 16:14:47 +0330

    three

--- Hashtable.cpp
+++ Hashtable.cpp
@@ -6,3 +6,17 @@ using namespace std;
 int main() {
   //initialize a string
   string s="geeksforgeeks";
+ int arr[26]={0};
+
+  //Storing the count
+  for(int i=0;i<s.size();i++){
+    arr[s[i]-'a']++;
+  }
+
+  //Search the count of the character
+  char ch='e';
+
+  // get count
+  cout<<"The count of ch is "<<arr[ch-'a']<<endl;
+  return 0;
+}
