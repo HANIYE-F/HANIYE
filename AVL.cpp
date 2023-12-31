25fe65048d16578f3ba523b00bca705fd90acb4f
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-31 21:14:07 +0330

    three

--- AVL.cpp
+++ AVL.cpp
@@ -14,3 +14,25 @@ int height(Node *N) {
         return 0;
     return N->height;
 }
+Node* newNode(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->height = 1;  // new node is initially added at leaf
+    return(node);
+}
+
+Node *rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
