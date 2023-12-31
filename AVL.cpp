c15ab472d106cff41033dc6d9e1809d862231300
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-31 21:18:55 +0330

    four

--- AVL.cpp
+++ AVL.cpp
@@ -35,4 +35,50 @@ Node *rightRotate(Node *y) {
 
     return x;
 }
+Node *leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
 
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+int getBalanceFactor(Node *N) {
+    if (N == nullptr)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+Node* insert(Node* node, int key) {
+    if (node == nullptr)
+        return(newNode(key));
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = 1 + max(height(node->left), height(node->right));
+
+    int balance = getBalanceFactor(node);
+
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    // Left Right Case
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
