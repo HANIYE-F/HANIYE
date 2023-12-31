0d1f00cffb88159e277f75f848f5ba97f48a3466
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-31 21:38:04 +0330

    ten

--- AVL.cpp
+++ AVL.cpp
@@ -17,3 +17,28 @@ int getHeight(Node* node)
         return 0;
     return node->height;
 }
+// تابع برای ایجاد گره جدید با داده مشخص شده بصورتی که اشاره گر ها به نال پیتیآر و ارتفاع گره برابر 1 باشد
+Node* createNode(int data)
+{
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    newNode->height = 1;
+    return newNode;
+}
+
+// تابع برای انجام عملیات چرخش به راست
+Node* rotateRight(Node* z)
+{
+    Node* y = z->left;
+    Node* T3 = y->right;
+
+    y->right = z;
+    z->left = T3;
+
+    z->height = 1 + max(getHeight(z->left), getHeight(z->right));
+    y->height = 1 + max(getHeight(y->left), getHeight(y->right));
+
+    return y;
+}
