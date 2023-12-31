ef69c4203b91cdccd8afbb760a18db97e4266e5e
HANIYE-F <fekrihaniye02@gmail.com>
2023-12-31 21:36:25 +0330

    nine

--- AVL.cpp
+++ AVL.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+// ساختار گره درخت AVL
+struct Node
+{
+    int data; // داده
+    Node* left; // اشاره‌گر به گره چپ
+    Node* right; // اشاره‌گر به گره راست
+    int height; // ارتفاع گره
+};
+
+// تابع برای محاسبه ارتفاع گره
+int getHeight(Node* node)
+{
+    if (node == nullptr)
+        return 0;
+    return node->height;
+}
